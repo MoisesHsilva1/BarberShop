0688dc5ef2a8c7ba8e4be6d20eff23f1d89c37b7
Moises Henrique <moihgm67@gmail.com>
2026-06-13 00:34:20 -0300

    feat: refactor Header component to replace HeaderNav with MenuItems for navigation

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,14 +1,22 @@
-import { HeaderNav } from "../molecules/HeaderNav";
+import { NavigationItemInterface } from "@/types/interface/NavigationItemInterface";
 import { NAVIGATION_ROUTES } from "../../config/enum/navigation";
+
 import Logo from "../atoms/Logo";
+import MenuItems from "../atoms/MenuItems";
 
 const Header = () => {
   return (
     <header>
       <nav className="bg-background h-24 border-b border-border">
         <div className="flex justify-between items-center h-full px-4 max-w-7xl mx-auto">
-          <Logo />
-          <HeaderNav routes={NAVIGATION_ROUTES} />
+          <Logo firstName="Nasck" lastName="Hair" />
+          <div className="flex gap-4 items-center">
+            {NAVIGATION_ROUTES.map((route: NavigationItemInterface) => (
+              <MenuItems key={route.href} to={route.href}>
+                {route.label}
+              </MenuItems>
+            ))}
+          </div>
         </div>
       </nav>
     </header>
